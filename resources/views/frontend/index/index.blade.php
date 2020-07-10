ae20ed9f3d9d78aeedd83ad743f37fc704669f2b
chenbing <chenbingbing@dazuhang.com>
2020-07-10 14:37:09 +0800

    swipper.css使用本地

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('css')
     <link crossorigin="anonymous" integrity="sha384-K6LrEaceM4QP87RzJ7R4CDXcFN4cFW/A5Q7/fEp/92c2WV+woVw9S9zKDO23sNS+"
-          href="https://lib.baomitu.com/Swiper/4.5.0/css/swiper.min.css" rel="stylesheet">
+          href="css/swiper.min.css" rel="stylesheet">
     <style>
         body {
             padding-top: 71px;
@@ -19,8 +19,8 @@
 
     <div class="container-fluid slider-box">
         <div class="container">
-            <!-- <div class="row"> -->
-                <!-- <div class="col-12"> -->
+            <div class="row">
+                <div class="col-12">
                     <div class="swiper-container">
                         <div class="swiper-wrapper">
                             @foreach($sliders as $slider)
@@ -31,7 +31,7 @@
                         </div>
                         <div class="swiper-pagination"></div>
                     </div>
-                <!-- </div> -->
+                </div>
                 @if($gAnnouncement)
                     <div class="col-12">
                         <div class="announcement-box">
@@ -39,7 +39,7 @@
                         </div>
                     </div>
                 @endif
-            <!-- </div> -->
+            </div>
         </div>
     </div>
 
